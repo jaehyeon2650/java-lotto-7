c9a6914edf273446760c7bcb377dba15372d055f
wogus <ind07152@naver.com>
2024-12-10 21:21:59 +0900

    feat(Parser): 입력 받은 문자를 파싱하는 기능 추가
    - 구매 금액에 대해 검증하고 구매 개수를 반환한다.
    - 로또 숫자들에 대해 검증하고 숫자들을 파싱하여 반환한다.
    - 보너스 숫자에 대해 검증하고 보너스 숫자를 반환한다.

---
+++ src/main/java/lotto/utils/Parser.java
@@ -0,0 +1,54 @@
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
+
+public class Parser {
+    public static int getCount(String input) {
+        Validator.validateIntegerType(input);
+        int money = Integer.parseInt(input);
+        Validator.validateMoneyUnit(money);
+        return money / 1000;
+    }
+
+    public static List<Integer> getNumbers(String input) {
+        Validator.validateNumbers(input);
+        return Arrays.stream(input.split(",")).map(word -> {
+            word = word.trim();
+            return Integer.parseInt(word);
+        }).toList();
+    }
+
+    public static int getBonusNumber(String input) {
+        Validator.validateIntegerType(input);
+        return Integer.parseInt(input);
+    }
+
+    private static class Validator {
+
+        private static final String EXPRESSION = "\\s*\\d+\\s*([,]\\s*\\d+\\s*)*";
+
+        public static void validateIntegerType(String money) {
+            if (!money.matches("\\d+")) {
+                throw LottoException.from(ErrorMessage.INVALID_INTEGER_TYPE);
+            }
+        }
+
+        public static void validateMoneyUnit(int money) {
+            if (money < 0 || money % 1000 != 0 || money / 1000 == 0) {
+                throw LottoException.from(ErrorMessage.INVALID_MONEY_UNIT);
+            }
+        }
+
+        public static void validateNumbers(String input) {
+            boolean matches = Pattern.matches(EXPRESSION, input);
+            if (!matches) {
+                throw LottoException.from(ErrorMessage.INVALID_LOTTO_INPUT);
+            }
+        }
+
+    }
+}
