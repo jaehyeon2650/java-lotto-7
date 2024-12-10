ff16be6a5c893e1019690108341fd2f0139f047d
wogus <ind07152@naver.com>
2024-12-10 20:00:07 +0900

    feat(LottoAnswer): 로또 정답을 가지는 도메인 추가
    - 로또 정답과 보너스 번호 정보를 저장하는 도메인을 추가한다.
    - 구매한 로또와 비교하여 결과를 반환하는 기능을 추가한다.

---
+++ src/main/java/lotto/domain/LottoAnswer.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoAnswer {
+    private final Lotto lotto;
+    private final BonusNumber bonusNumber;
+
+    public LottoAnswer(List<Integer> numbers, int bonusNumber) {
+        this.lotto = new Lotto(numbers);
+        this.bonusNumber = new BonusNumber(bonusNumber);
+    }
+
+    public Map<Result, Integer> lottoResult(Lottos lottos) {
+        Map<Result, Integer> results = new EnumMap<>(Result.class);
+        List<Lotto> buyLottos = lottos.getLottos();
+        for (Lotto buyLotto : buyLottos) {
+            int matchedCount = lotto.getMatchedCount(buyLotto);
+            boolean bonus = buyLotto.hasNumber(bonusNumber.getNumber());
+            Result result = Result.findResult(matchedCount, bonus);
+            results.put(result, results.getOrDefault(result, 0) + 1);
+        }
+        return results;
+
+    }
+}
