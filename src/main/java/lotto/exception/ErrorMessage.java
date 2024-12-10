db29afba2591fc8679af69191a41e2b247633b0f
wogus <ind07152@naver.com>
2024-12-10 21:26:47 +0900

    feat(ErrorMessage): 에러 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,7 +3,10 @@ package lotto.exception;
 public enum ErrorMessage {
     INVALID_LOTTO_SIZE("로또 번호는 6개여야 합니다."),
     INVALID_LOTTO_RANGE("로또 번호는 1부터 45 사이의 숫자여야합니다."),
-    INVALID_LOTTO_DUPLICATE("로또 번호는 중복이 되면 안됩니다.");
+    INVALID_LOTTO_DUPLICATE("로또 번호는 중복이 되면 안됩니다."),
+    INVALID_INTEGER_TYPE("숫자만 입력해주세요."),
+    INVALID_MONEY_UNIT("금액은 1000원 단위로만 가능합니다(최소 1개)"),
+    INVALID_LOTTO_INPUT("로또 번호를 ,를 기준으로 정확히 입력해주세요");
     private final String message;
 
     ErrorMessage(String message) {
