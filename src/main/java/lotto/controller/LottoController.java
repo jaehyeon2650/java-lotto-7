ff3b0e35a86819691e7f0833d78e9a1096dd3daf
wogus <ind07152@naver.com>
2024-12-10 22:00:46 +0900

    refactor(LottoController): 반복 로직 리펙토링
    함수형 인터페이스를 통해 반복되는 재입력을 리펙토링한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.dto.LottosDto;
@@ -22,22 +24,16 @@ public class LottoController {
     }
 
     public void start() {
-        Lottos buyLotto = buyLotto();
+        Lottos buyLotto = retryInvalidInput(this::buyLotto);
         showLotto(buyLotto);
-        Lotto lotto = getAnswerLotto();
-        ResultDto result = getResult(buyLotto, lotto);
+        Lotto lotto = retryInvalidInput(this::getAnswerLotto);
+        ResultDto result = retryInvalidInput(this::getResult, buyLotto, lotto);
         printResult(result);
     }
 
     private Lottos buyLotto() {
-        while (true) {
-            try {
-                int count = inputView.getCount();
-                return lottoService.buyLotto(count);
-            } catch (LottoException e) {
-                outputView.printError(e.getMessage());
-            }
-        }
+        int count = inputView.getCount();
+        return lottoService.buyLotto(count);
     }
 
     private void showLotto(Lottos lottos) {
@@ -45,28 +41,36 @@ public class LottoController {
     }
 
     private Lotto getAnswerLotto() {
-        while (true) {
-            try {
-                List<Integer> numbers = inputView.getNumbers();
-                return lottoService.makeAnswerLotto(numbers);
-            } catch (LottoException e) {
-                outputView.printError(e.getMessage());
-            }
-        }
+        List<Integer> numbers = inputView.getNumbers();
+        return lottoService.makeAnswerLotto(numbers);
     }
 
     private ResultDto getResult(Lottos butLottos, Lotto lotto) {
-        while (true) {
-            try {
-                int bonusNumber = inputView.getBonusNumber();
-                return lottoService.getResult(butLottos, lotto, bonusNumber);
-            } catch (LottoException e) {
-                outputView.printError(e.getMessage());
-            }
-        }
+        int bonusNumber = inputView.getBonusNumber();
+        return lottoService.getResult(butLottos, lotto, bonusNumber);
     }
 
     private void printResult(ResultDto result) {
         outputView.printResult(result);
+    }
+
+    private <T> T retryInvalidInput(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (LottoException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private <T, U, R> R retryInvalidInput(BiFunction<T, U, R> biFunction, T t, U u) {
+        while (true) {
+            try {
+                return biFunction.apply(t, u);
+            } catch (LottoException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
     }
 }
